fbe91beee1aeb40bf6c98ff63cf8bc37f4692aad
sutap2000 <sutappandey@gmail.com>
2023-12-07 18:47:33 +0530

    Removed password encoder and hardcoded User Details in another Service class

--- spring-security/src/main/java/com/spring/security/SecurityConfiguration.java
+++ spring-security/src/main/java/com/spring/security/SecurityConfiguration.java
@@ -1,42 +1,25 @@
 package com.spring.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 	
-	@Bean
-	UserDetailsService userDetailsService(PasswordEncoder encoder) {
-		UserDetails user = User
-				.withUsername("suresh")
-				.password(encoder.encode("ramesh"))
-				.roles("USER")
-				.build();
-				
-		UserDetails admin = User
-				.withUsername("khalnayak")
-				.password(encoder.encode("keetnashak"))
-				.roles("ADMIN")
-				.build();
-		
-		return new InMemoryUserDetailsManager(user, admin);
-				
-	}
+	@Autowired UserDetailsService userDetailsService;
 	
 	@Bean
 	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+		return NoOpPasswordEncoder.getInstance();
 	}
 	
 	@Bean
